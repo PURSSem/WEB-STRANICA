492570e81bd0cba4ead6de17f2b63c65d71e398d
hrvojekolar <hrvojekolar1998@gmail.com>
2022-01-11 00:25:45 +0100

    Dodan logout

--- includes/logout.inc.php
+++ includes/logout.inc.php
@@ -1,28 +1,8 @@
 <?php
-include_once 'header.php';
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<body>
-    <h1>Hotel PaKoPa</h1>
-    <h1>Dobrodošli</h1>
-        <form action="index.php">
-        <input type="submit" value="Početna">
-        </form>
-        <form action="onama.php">
-        <input type="submit" value="O nama">
-        </form>
-        <form action="kontakt.php">
-        <input type="submit" value="Kontakt">
-        </form>
-        <form action="prijava.php">
-        <input type="submit" value="Prijava">
-        </form>
-        <form action="registracija.php">
-        <input type="submit" value="Registracija">
-        </form>
+session_start();
+session_unset();
+session_destroy();
 
-        <?php
-include_once 'footer.php';
-?>
+header("location: ../index.php?error=wronglogin");
+exit();
