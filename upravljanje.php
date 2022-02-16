a52b9d62f7d67e323c319f154794819c46d4fc8f
Ante Pavkovic <ante.pavkovic98@gmail.com>
2022-02-16 11:24:15 +0100

    Dodan mqtt

--- upravljanje.php
+++ upravljanje.php
@@ -1,5 +1,85 @@
+<?php
+require('C:\Users\antep\vendor\autoload.php');
+
+use \PhpMqtt\Client\MqttClient;
+use \PhpMqtt\Client\ConnectionSettings;
+
+$server   = 'broker.emqx.io';
+$port     = 1883;
+$clientId = 'Ante';
+$username = null;
+$password = null;
+$clean_session = false;
+
+$connectionSettings  = new ConnectionSettings();
+$connectionSettings
+    ->setUsername($username)
+    ->setPassword(null)
+    ->setKeepAliveInterval(60)
+    ->setLastWillTopic('emqx/test/last-will')
+    ->setLastWillMessage('client disconnect')
+    ->setLastWillQualityOfService(1);
+//pcntl_async_signals(true);
+
+$mqtt = new MqttClient($server, $port, $clientId);
+//pcntl_signal(SIGINT, function (int $signal, $info) use ($mqtt) {
+  //  $mqtt->interrupt();
+//});
+$mqtt->connect($connectionSettings, $clean_session);
+
+$mqtt->subscribe('soba101/temptest', function ($topic, $message) {
+    printf("Received message on topic [%s]: %s\n", $topic, $message);
+
+    echo "<h2>" . $message . "</h2>";
+    echo "<h2>" . $topic . "</h2>";
+}, 0);
+
+
+if (array_key_exists('wc101ON', $_POST)) {
+    $payload = array(
+        'on'
+    );
+    $mqtt->publish(
+        // topic
+        'soba101/wc',
+        // payload
+        'on',
+        // qos
+        0,
+        // retain
+        true
+    );
+}
+
+if (array_key_exists('wc101OFF', $_POST)) {
+    $payload = array(
+        'off'
+    );
+    $mqtt->publish(
+        // topic
+        'soba101/wc',
+        // payload
+        'off',
+        // qos
+        0,
+        // retain
+        true
+    );
+}
+
+//sleep(1);
+//$mqtt->loop(true);
+
+
+
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <link rel="stylesheet" href="stylesupravljanje.css">
     <meta charset="UTF-8">
@@ -12,19 +92,33 @@
     <center>
         <h1>Upravljanje hotelskom sobom</h1>
 
-        <h3>Svjetlo WC <input type="button" name="SvjetloWcButton" value="OFF"></h3>
+        <form method="post">
 
-        <h3>Svjetlo soba <input type="button" name="SvjetloSobaButton" value="OFF"></h3> 
+            <h3>rasvjeta kupaonica</h3>
+            <input type="submit" name="wc101ON" class="button" value="ON" />
+            <input type="submit" name="wc101OFF" class="button" value="OFF" />
 
-        <h3>Svjetlo krevet <input type="button" name="SvjetloKrevetButton" value="OFF"></h3>
+            <h3>Rasvjeta soba</h3>
+            <input type="submit" name="soba101ON" class="button" value="ON" />
+            <input type="submit" name="soba101OFF" class="button" value="OFF" />
 
-        <h3>Vrata <input type="button" name="VrataButton" value="OTKLJUČANO"></h3>
+            <h3>Vrata</h3>
+            <input type="submit" name="vrata101" class="button" value="Otključaj" />
 
-        <h3>Rolete <input type="button" name="RoleteGore" value="GORE">     <input type="button" name="RoleteDolje" value="DOLJE"></h3>
+            <h3>Rolete</h3>
+            <input type="submit" name="roletegore" class="button" value="&#8593" />
+            <input type="submit" name="roletedolje" class="button" value="&#8595" />
 
-        <h3>Tempreratura</h3>
-        
-        <input type="button" name="TempGore" style="font-size:150%;" value="+">     <input type="button" name="TempDolje" style="font-size:150%;"" value="-"></h3>
+
+            <h3>Temperatura</h3>
+
+            <input type="submit" name="tempgore" class="button" value="&#8593" />
+            <input type="submit" name="tempdolje" class="button" value="&#8595" />
+
+            <h3>Stanje temperature</h3>
+
+
+
     </center>
     <div>
         <br>
