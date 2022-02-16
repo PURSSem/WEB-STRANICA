e85ce46ed08855ad57d220bc8bdb2e1fdad26824
hrvojekolar <hrvojekolar1998@gmail.com>
2022-02-16 13:23:44 +0100

    Dodan Admin privleges

--- onama.php
+++ onama.php
@@ -23,13 +23,16 @@ include_once 'header.php';
         </form>
         <?php
         if(isset($_SESSION["username"])){
-          echo "<li><a href='upravljanje.php'>Upravljanje</a></li>";
-          echo "<li><a href='includes/logout.inc.php'>Odjava</a></li>";
-      }
-      else{
-        echo "<li><a href='prijava.php'>Prijava</a></li>";
-        echo "<li><a href='registracija.php'>Registracija</a></li>";
-      }
+          if(($_SESSION["username"])=="admin"){
+           echo "<li><a href='registracija.php'>Registracija</a></li>";
+           echo "<li><a href='includes/logout.inc.php'>Odjava</a></li>";
+         }else if(($_SESSION["username"])!="admin"){
+           echo "<li><a href='upravljanje.php'>Upravljanje</a></li>";
+           echo "<li><a href='includes/logout.inc.php'>Odjava</a></li>";
+         }
+         }else{
+           echo "<li><a href='prijava.php'>Prijava</a></li>";
+         }
         ?>
     <p>
         Hotel PaKoPa je sustav razvijen od strane studenata specijalističkog studija 
--- registracija.php
+++ registracija.php
@@ -29,14 +29,9 @@ include_once 'header.php';
         <input type="submit" value="Kontakt">
         </form>
         <?php
-      if(isset($_SESSION["username"])){
-          echo "<li><a href='upravljanje.php'>Upravljanje</a></li>";
-          echo "<li><a href='includes/logout.inc.php'>Odjava</a></li>";
-      }
-      else{
-        echo "<li><a href='prijava.php'>Prijava</a></li>";
-        echo "<li><a href='registracija.php'>Registracija</a></li>";
-      }
+     
+        echo "<li><a href='includes/logout.inc.php'>Odjava</a></li>";
+        
         ?>
     <p>
         Registracija korisnika.
